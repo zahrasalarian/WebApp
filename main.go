5a7908584c3ac38d78bcb7e102d6cb422277a149
amberbrown4 <zasalarian2000@gmail.com>
2020-08-01 21:56:57 +0430

    Added a tbbt page

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request)  {
-  fmt.Fprint(w, "<h1>fg</h1>")
+  w.Header().Set("Content-Type","text/html")
+  if r.URL.Path == "/"{
+    fmt.Fprint(w, "<h1>Hello</h1>")
+  }else if r.URL.Path == "/tbbt"{
+    fmt.Fprint(w, "Check it out <a href=\"https://en.wikipedia.org/wiki/The_Big_Bang_Theory\">The big bang theory</a>.")
+  }
 }
 
 func main()  {
