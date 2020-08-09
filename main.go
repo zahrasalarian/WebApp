06f1a036c3d807cf9433711f5a3456931eadf064
amberbrown4 <zasalarian2000@gmail.com>
2020-08-09 23:16:03 +0430

    Added hello to whoever funtionality

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "net/http"
+  "github.com/julienschmidt/httprouter"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request)  {
@@ -11,10 +12,19 @@ func handlerFunc(w http.ResponseWriter, r *http.Request)  {
     fmt.Fprint(w, "<h1>Hello</h1>")
   }else if r.URL.Path == "/tbbt"{
     fmt.Fprint(w, "Check it out <a href=\"https://en.wikipedia.org/wiki/The_Big_Bang_Theory\">The big bang theory</a>.")
+  }else{
+    fmt.Fprint(w, "<h1>Sorry, you're lost</h1>")
   }
 }
 
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
 func main()  {
-  http.HandleFunc("/", handlerFunc)
-  http.ListenAndServe(":2000", nil)
+  router := httprouter.New()
+  router.GET("/hello/:name", Hello)
+
+  // http.HandleFunc("/", handlerFunc)
+  http.ListenAndServe(":2000", router)
 }
