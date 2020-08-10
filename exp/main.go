217c00c2bacfd54922d6430dbacfac208006de69
amberbrown4 <zasalarian2000@gmail.com>
2020-08-10 22:49:03 +0430

    Added rendering with nested structues feature

--- exp/main.go
+++ exp/main.go
@@ -3,14 +3,24 @@ package main
 import(
   "os"
   "html/template"
+  // "fmt"
 )
 
 type User struct{
   Name string
-  Dog string
+  Dog DogInf
 }
+type DogInf struct{
+    Name string
+    Age int
+}
+type CastsInf struct {
+      CastsName []string
+ }
 
 func main()  {
+
+  // casts := []string{"Johnny Galecki","Jim Parsons","Kaley Cuoco","Simon Helberg","Kunal Nayyar","Mayim Bialik","Melissa Rauch"}
   t, err := template.ParseFiles("hello.gohtml")
   if err != nil{
     panic(err)
@@ -18,11 +28,22 @@ func main()  {
 
   data := User{
     Name: "Amy Farrah Fowler",
-    Dog: "Cinnamon",
+    Dog: DogInf{
+      Name: "Cinnamon",
+      Age: 3,
+    },
   }
+  // castsInf := CastsInf{
+  //   CastsName: casts,
+  // }
 
   err = t.Execute(os.Stdout,data)
   if err != nil{
     panic(err)
   }
+  // err = t.Execute(os.Stdout,castsInf)
+  // if err != nil{
+  //   panic(err)
+  // }
+  // fmt.Println(casts)
 }
