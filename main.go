1ce8f97879f392eaf9b64d8559489888c14b26cb
amberbrown4 <zasalarian2000@gmail.com>
2020-08-10 19:25:41 +0430

    Implemented the Gorilla/mux router

--- main.go
+++ main.go
@@ -3,28 +3,24 @@ package main
 import (
   "fmt"
   "net/http"
-  "github.com/julienschmidt/httprouter"
+  // "github.com/julienschmidt/httprouter"
+  "github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request)  {
-  w.Header().Set("Content-Type","text/html")
-  if r.URL.Path == "/"{
-    fmt.Fprint(w, "<h1>Hello</h1>")
-  }else if r.URL.Path == "/tbbt"{
-    fmt.Fprint(w, "Check it out <a href=\"https://en.wikipedia.org/wiki/The_Big_Bang_Theory\">The big bang theory</a>.")
-  }else{
-    fmt.Fprint(w, "<h1>Sorry, you're lost</h1>")
-  }
+
+func home(w http.ResponseWriter, r *http.Request)  {
+  w.Header().Set("Content-Type", "text/html")
+  fmt.Fprint(w, "<h1>Hello</h1>")
 }
 
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-    fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+func tbbt(w http.ResponseWriter, r *http.Request)  {
+  w.Header().Set("Content-Type", "text/html")
+  fmt.Fprint(w, "Check it out <a href=\"https://en.wikipedia.org/wiki/The_Big_Bang_Theory\">The big bang theory</a>.")
 }
 
 func main()  {
-  router := httprouter.New()
-  router.GET("/hello/:name", Hello)
-
-  // http.HandleFunc("/", handlerFunc)
-  http.ListenAndServe(":2000", router)
+  r := mux.NewRouter()
+  r.HandleFunc("/", home)
+  r.HandleFunc("/tbbt", tbbt)
+  http.ListenAndServe(":2000", r)
 }
