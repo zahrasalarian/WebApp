b1c5de81655ddddf2a7b9f752979096d499277a8
amberbrown4 <zasalarian2000@gmail.com>
2020-08-13 21:58:22 +0430

    Created simle tbbt view

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-  "fmt"
+  // "fmt"
   "net/http"
   "html/template"
   // "github.com/julienschmidt/httprouter"
   "github.com/gorilla/mux"
 )
 
-var homeTemplate *template.Template
+var(
+  homeTemplate *template.Template
+  tbbtTemplate *template.Template
+)
 func home(w http.ResponseWriter, r *http.Request)  {
   w.Header().Set("Content-Type", "text/html")
   if err := homeTemplate.Execute(w,nil); err != nil{
@@ -18,12 +21,18 @@ func home(w http.ResponseWriter, r *http.Request)  {
 
 func tbbt(w http.ResponseWriter, r *http.Request)  {
   w.Header().Set("Content-Type", "text/html")
-  fmt.Fprint(w, "Check it out <a href=\"https://en.wikipedia.org/wiki/The_Big_Bang_Theory\">The big bang theory</a>.")
+  if err := tbbtTemplate.Execute(w,nil); err != nil{
+    panic(err)
+  }
 }
 
 func main()  {
   var err error
   homeTemplate, err = template.ParseFiles("views/home.gohtml")
+  if err != nil{
+    panic(err)
+  }
+  tbbtTemplate, err = template.ParseFiles("views/tbbt.gohtml")
   if err != nil{
     panic(err)
   }
